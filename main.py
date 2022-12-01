820e272f11f4022ad769b1c7461c68c7f867f692
Jeffrey <geoff213454@gmail.com>
2022-11-30 20:37:32 -0500

    bigupdate

--- main.py
+++ main.py
@@ -1,97 +1,24 @@
 import pandas as pd
-import numpy as np
-from geopy.geocoders import Nominatim
 import matplotlib.pyplot as plt
 import seaborn as sns
 from matplotlib import ticker
 from matplotlib.gridspec import GridSpec
- 
-def preprocess_bls(bls_df):
-    bls_df = bls_df.replace('*','')
-    bls_df = bls_df.replace('**','')
+import numpy as np
+import plotly.graph_objects as go
 
-    replace_hashtag_columns = ['H_MEAN', 'H_PCT10', 'H_PCT25', 'H_MEDIAN', 'H_PCT75', 'H_PCT90']
-    for column in replace_hashtag_columns:
-        bls_df[column] = bls_df[column].str.replace('#','100')
-        
-    replace_hashtag_columns2 = ['A_MEAN', 'A_PCT10', 'A_PCT25', 'A_MEDIAN', 'A_PCT75', 'A_PCT90']
-    for column in replace_hashtag_columns2:
-        bls_df[column] = bls_df[column].str.replace('#','208000')
-        
-    bls_df['PCT_RPT'] = bls_df['PCT_RPT'].str.replace('~','0.5')
+msa_df = pd.read_csv('msa.csv')
 
-    remove_commas_columns = ['TOT_EMP', 'A_MEAN', 'A_PCT10', 'A_PCT25', 'A_MEDIAN', 'A_PCT75', 'A_PCT90']
-    for column in remove_commas_columns:
-        bls_df[column] = bls_df[column].str.replace(',','')
-        
-    make_numeric_columns = ['TOT_EMP', 'EMP_PRSE', 'JOBS_1000', 'LOC_QUOTIENT',
-    'PCT_TOTAL', 'PCT_RPT','H_MEAN', 'A_MEAN', 'MEAN_PRSE',
-    'H_PCT10', 'H_PCT25', 'H_MEDIAN', 'H_PCT75', 'H_PCT90',
-    'A_PCT10', 'A_PCT25', 'A_MEDIAN', 'A_PCT75', 'A_PCT90']
-
-    for column in make_numeric_columns:
-        bls_df[column] = pd.to_numeric(bls_df[column])
+def get_metropolitan_areas(occupation, msa_df = msa_df):
+    df = msa_df.query('OCC_TITLE == "{}"'.format(occupation))
+    df = df.drop('OCC_TITLE', axis=1)
     
-    return bls_df
-
-def preprocess_bea(bea_df):
-    bea_df = bea_df.iloc[2:]
-    bea_df = bea_df.drop('GeoFips', axis=1)
-    bea_df = bea_df.rename(columns={'GeoName': 'AREA_TITLE', '2020': 'RPP'})
-    bea_df['AREA_TITLE'] = bea_df['AREA_TITLE'].str.replace(' \(Metropolitan Statistical Area\)','', regex=True)
-    return bea_df
-
-bls_df = pd.read_csv('bls.csv', dtype = {'AREA': 'str', 'AREA_TYPE': 'str', 'OWN_CODE': 'str', 'NAICS': 'str'}, low_memory=False)
-bls_df = preprocess_bls(bls_df)
-bea_df = pd.read_csv('bea.csv', header=3, engine='python', skipfooter=3)
-bea_df = preprocess_bea(bea_df)
-
-def add_rpp(bls_df, bea_df):
-    merged_df = pd.merge(bls_df, bea_df, on ='AREA_TITLE', how ='left')
-
-    for index1, row1 in merged_df.iterrows():
-        if np.isnan(row1['RPP']):
-            max_match = 0
-            cities1, states1 = row1['AREA_TITLE'].split(', ')
-            s1 = set(states1.split('-'))
-            c1 = set(cities1.split('-'))
-            for index2, row2 in bea_df.iterrows():
-                cities2, states2 = row2['AREA_TITLE'].split(', ')
-                s2 = set(states2.split('-'))
-                c2 = set(cities2.split('-'))
-                current_match = len(s1.intersection(s2)) + len(c1.intersection(c2))
-                if current_match > max_match:
-                    max_match = current_match
-                    merged_df.at[index1,'RPP'] = row2['RPP']
-    return merged_df
-
-#def add_coordinates(bls_df):
-    
-
-def get_top_metropolitan_areas(occupation, bls_df, bea_df):
-    bls_df = bls_df.query('AREA_TYPE == "4" and OCC_TITLE == "{}"'.format(occupation))
-    bls_df = bls_df[['AREA_TITLE', 'TOT_EMP', 'LOC_QUOTIENT', 'A_MEAN', 'A_MEDIAN']]
-    merged_df = add_rpp(bls_df, bea_df)
-    for statistic in ['A_MEAN', 'A_MEDIAN']:
-        merged_df['RPP_ADJUSTED_'+statistic] = merged_df[statistic]/merged_df['RPP']*100
-    merged_df = merged_df.drop('RPP', axis = 1)
-        
-    column_names = list(merged_df.columns)
-    column_names.remove('AREA_TITLE')
+    column_names = ['TOT_EMP', 'LOC_QUOTIENT', 'A_MEAN', 'A_MEDIAN', 'RPP_ADJUSTED_A_MEAN', 'RPP_ADJUSTED_A_MEDIAN']
     for column_name in column_names:
-        merged_df[column_name+'_Z_SCORE'] = (merged_df[column_name] - merged_df[column_name].mean())/merged_df[column_name].std()
-    merged_df['AVERAGE_Z_SCORE'] = merged_df[[column_name+'_Z_SCORE' for column_name in column_names]].mean(axis=1)
+        df[column_name+'_Z_SCORE'] = (df[column_name] - df[column_name].mean())/df[column_name].std()
+    df['AVERAGE_Z_SCORE'] = df[[column_name+'_Z_SCORE' for column_name in column_names]].mean(axis=1)
     for column_name in column_names:
-        merged_df = merged_df.drop(column_name+'_Z_SCORE', axis=1)
-    #return merged_df
-    for index, row in bls_df.iterrows():
-        cities, states = row['AREA_TITLE'].split(', ')
-        first_city = cities.split('-')[0]
-        first_state = states.split('-')[0]
-        loc = Nominatim(user_agent='GetLoc')
-        getLoc = loc.geocode(first_city+', '+first_state)        
-        print(first_city, first_state, getLoc.latitude, getLoc.longitude)
-
+        df = df.drop(column_name+'_Z_SCORE', axis=1)
+    return df
 
 @ticker.FuncFormatter
 def kmbt_format(num, pos):
@@ -102,14 +29,14 @@ def kmbt_format(num, pos):
         num /= 1000.0
     return '{}{}'.format('{:f}'.format(num).rstrip('0').rstrip('.'), ['', 'K', 'M', 'B', 'T'][magnitude])
 
-def visualize_top_metropolitan_areas(occupation, bls_df, bea_df):
+def visualize_metropolitan_areas(occupation):
     names = {'TOT_EMP':'Total Employment', 
              'LOC_QUOTIENT':'Location Quotient', 
              'A_MEAN':'Mean Annual Wage',
              'A_MEDIAN':'Annual Median Wage',
              'RPP_ADJUSTED_A_MEAN': 'RPP-Adjusted Mean Annual Wage',
              'RPP_ADJUSTED_A_MEDIAN': 'RPP-Adjusted Annual Median Wage'}
-    df = get_top_metropolitan_areas(occupation, bls_df, bea_df)
+    df = get_metropolitan_areas(occupation)
     fig = plt.figure(tight_layout=True)
     gs = GridSpec(5, 2, figure=fig)
     fig.set_figheight(13)
@@ -134,19 +61,35 @@ def visualize_top_metropolitan_areas(occupation, bls_df, bea_df):
     fig.suptitle('Best Metropolitan Areas For '+occupation)
     plt.show()
 
+def map_metropolitan_areas(df):
+    df = df.dropna()
+    fig = go.Figure(data=go.Scattergeo(
+            lon = df['LONGITUDE'],
+            lat = df['LATTITUDE'],
+            hoverinfo='text',
+            text = df['AREA_TITLE'],
+            marker = dict(
+                sizemode = 'area',
+                size = df['TOT_EMP']**0.5*4,
+                color = df['AVERAGE_Z_SCORE'],
+                colorscale = 'viridis'
+                ),
+            ))
+    fig.update_layout(geo_scope='usa')
+    return fig
 
-get_top_metropolitan_areas('Data Scientists', bls_df, bea_df)
-'''
 from dash import Dash, html, dcc, Input, Output
 import plotly.express as px
 
 app = Dash(__name__)
 
-def generate_figures(occupation, bls_df, bea_df):
-    df = get_top_metropolitan_areas(occupation, bls_df, bea_df)
-    ranking = df[['AREA_TITLE', 'AVERAGE_Z_SCORE']].sort_values('AVERAGE_Z_SCORE', ascending = False)
+def generate_figures(occupation):
     figs = []
-    fig = px.bar(ranking.head(10), x='AVERAGE_Z_SCORE', y='AREA_TITLE',  height = 500)
+    df = get_metropolitan_areas(occupation)
+    
+    figs.append(map_metropolitan_areas(df))
+    ranking = df[['AREA_TITLE', 'AVERAGE_Z_SCORE']].sort_values('AVERAGE_Z_SCORE', ascending = False)
+    fig = px.bar(ranking.head(10), x='AVERAGE_Z_SCORE', y='AREA_TITLE', height = 500)
     fig.update_layout(yaxis={'categoryorder':'array', 'categoryarray':ranking.iloc[::-1]['AREA_TITLE']}, title='Highest Average Z-Score', title_x = 0.5, xaxis_title='', yaxis_title = '')
     fig.update_traces(hovertemplate='Metropolitan area: %{y} <br>Average z-score: %{x}')
     fig.update_layout(
@@ -173,11 +116,8 @@ def generate_figures(occupation, bls_df, bea_df):
         figs.append(fig)
     return figs
 
-def get_occupations(bls_df):
-    bls_df = bls_df.query('AREA_TYPE == "4"')
-    return bls_df['OCC_TITLE'].unique()
-
 @app.callback(
+    Output('map', 'figure'),
     Output('overall', 'figure'),
     Output('total-employment', 'figure'),
     Output('mean', 'figure'),
@@ -187,32 +127,33 @@ def get_occupations(bls_df):
     Output('median-adjusted', 'figure'),
     Input('dropdown', 'value')
 )
-def update_output(value, bls_df = bls_df, bea_df = bea_df):
-    figs = generate_figures(value, bls_df, bea_df)
-    return figs[0], figs[1], figs[3], figs[5], figs[2], figs[4], figs[6]
+def update_output(value):
+    figs = generate_figures(value)
+    return figs[0], figs[1], figs[2], figs[4], figs[6], figs[3], figs[5], figs[7]
 
-figs = generate_figures('Mathematicians', bls_df, bea_df)
+figs = generate_figures('Data Scientists')
 app.layout = html.Div([
     html.Div([
         html.Div(children=[
             html.H1(children = 'Best Metropolitan Areas for', style = {'font-family':'arial', 'margin-right':'0.67em', 'font-weight': 'normal', 'font-size': 16}),
-            dcc.Dropdown(get_occupations(bls_df), 'Mathematicians', style = {'font-family':'arial', 'flex': 1}, id='dropdown')
+            dcc.Dropdown(msa_df['OCC_TITLE'].unique(), 'Data Scientists', style = {'font-family':'arial', 'flex': 1}, id='dropdown')
         ], style={'display': 'flex', 'flex-direction': 'row', 'align-items': 'center', 'margin-bottom': 50}),
-        dcc.Graph(figure=figs[0], id='overall'),
+        dcc.Graph(figure=figs[0], id='map'),
+        dcc.Graph(figure=figs[1], id='overall'),
         html.Div([
             html.Div(children=[
-                dcc.Graph(figure=figs[1], id='total-employment'),
-                dcc.Graph(figure=figs[3], id='mean'),
-                dcc.Graph(figure=figs[5], id='mean-adjusted')
+                dcc.Graph(figure=figs[2], id='total-employment'),
+                dcc.Graph(figure=figs[4], id='mean'),
+                dcc.Graph(figure=figs[6], id='mean-adjusted')
             ], style={'flex': 0.5}),
             html.Div(children=[
-                dcc.Graph(figure=figs[2], id='location-quotient'),
-                dcc.Graph(figure=figs[4], id='median'),
-                dcc.Graph(figure=figs[6], id='median-adjusted')
+                dcc.Graph(figure=figs[3], id='location-quotient'),
+                dcc.Graph(figure=figs[5], id='median'),
+                dcc.Graph(figure=figs[7], id='median-adjusted')
             ], style={'flex': 0.5}) 
         ], style={'display': 'flex', 'flex-direction': 'row'})
     ], style = {'margin-top':100, 'margin-bottom':100, 'margin-left': 200, 'margin-right': 200})
 ])
 
 if __name__ == '__main__':
-    app.run_server(debug=True)'''
+    app.run_server(debug=True)
---
+++ preprocess.py
@@ -0,0 +1,91 @@
+import pandas as pd
+import numpy as np
+from geopy.geocoders import Nominatim
+ 
+def preprocess_bls(bls_df):
+    bls_df = bls_df.replace('*','')
+    bls_df = bls_df.replace('**','')
+
+    replace_hashtag_columns = ['H_MEAN', 'H_PCT10', 'H_PCT25', 'H_MEDIAN', 'H_PCT75', 'H_PCT90']
+    for column in replace_hashtag_columns:
+        bls_df[column] = bls_df[column].str.replace('#','100')
+        
+    replace_hashtag_columns2 = ['A_MEAN', 'A_PCT10', 'A_PCT25', 'A_MEDIAN', 'A_PCT75', 'A_PCT90']
+    for column in replace_hashtag_columns2:
+        bls_df[column] = bls_df[column].str.replace('#','208000')
+        
+    bls_df['PCT_RPT'] = bls_df['PCT_RPT'].str.replace('~','0.5')
+
+    remove_commas_columns = ['TOT_EMP', 'A_MEAN', 'A_PCT10', 'A_PCT25', 'A_MEDIAN', 'A_PCT75', 'A_PCT90']
+    for column in remove_commas_columns:
+        bls_df[column] = bls_df[column].str.replace(',','')
+        
+    make_numeric_columns = ['TOT_EMP', 'EMP_PRSE', 'JOBS_1000', 'LOC_QUOTIENT',
+    'PCT_TOTAL', 'PCT_RPT','H_MEAN', 'A_MEAN', 'MEAN_PRSE',
+    'H_PCT10', 'H_PCT25', 'H_MEDIAN', 'H_PCT75', 'H_PCT90',
+    'A_PCT10', 'A_PCT25', 'A_MEDIAN', 'A_PCT75', 'A_PCT90']
+
+    for column in make_numeric_columns:
+        bls_df[column] = pd.to_numeric(bls_df[column])
+    
+    return bls_df
+
+def preprocess_bea(bea_df):
+    bea_df = bea_df.iloc[2:]
+    bea_df = bea_df.drop('GeoFips', axis=1)
+    bea_df = bea_df.rename(columns={'GeoName': 'AREA_TITLE', '2020': 'RPP'})
+    bea_df['AREA_TITLE'] = bea_df['AREA_TITLE'].str.replace(' \(Metropolitan Statistical Area\)','', regex=True)
+    bea_df['AREA_TITLE'] = bea_df['AREA_TITLE'].str.replace(' 1/', '')
+    return bea_df
+
+bls_df = pd.read_csv('bls.csv', dtype = {'AREA': 'str', 'AREA_TYPE': 'str', 'OWN_CODE': 'str', 'NAICS': 'str'}, low_memory=False)
+bls_df = preprocess_bls(bls_df)
+bea_df = pd.read_csv('bea.csv', header=3, engine='python', skipfooter=3)
+bea_df = preprocess_bea(bea_df)
+
+def add_rpp(msa_df, bea_df=bea_df):
+    df = pd.DataFrame(msa_df['AREA_TITLE'].unique(), columns =['AREA_TITLE'])
+    rpp_df = pd.merge(df, bea_df, on ='AREA_TITLE', how ='left')
+    for index1, row1 in rpp_df.iterrows():
+        if np.isnan(row1['RPP']):
+            max_match = 0
+            cities1, states1 = row1['AREA_TITLE'].split(', ')
+            s1 = set(states1.split('-'))
+            c1 = set(cities1.split('-'))
+            for index2, row2 in bea_df.iterrows():
+                cities2, states2 = row2['AREA_TITLE'].split(', ')
+                s2 = set(states2.split('-'))
+                c2 = set(cities2.split('-'))
+                if s1.intersection(s2) and c1.intersection(c2):
+                    current_match = len(s1.intersection(s2)) + len(c1.intersection(c2))
+                    if current_match > max_match:
+                        rpp_df.at[index1, 'RPP'] = row2['RPP']
+                        max_match = current_match
+    msa_df = pd.merge(msa_df, rpp_df, on ='AREA_TITLE', how ='left')
+    for statistic in ['A_MEAN', 'A_MEDIAN']:
+        msa_df['RPP_ADJUSTED_'+statistic] = msa_df[statistic]/msa_df['RPP']*100
+    msa_df = msa_df.drop('RPP', axis = 1)
+    return msa_df
+
+def add_coordinates(msa_df):
+    coordinates_df = pd.DataFrame(msa_df['AREA_TITLE'].unique(), columns =['AREA_TITLE'])
+    latitudes = []
+    longitudes = []
+    for index, row in coordinates_df.iterrows():
+        cities, states = row['AREA_TITLE'].split(', ')
+        first_city = cities.split('-')[0]
+        first_state = states.split('-')[0]
+        loc = Nominatim(user_agent='GetLoc')
+        getLoc = loc.geocode(first_city+', '+first_state)
+        latitudes.append(getLoc.latitude)
+        longitudes.append(getLoc.longitude)
+        print(first_city, first_state, getLoc.latitude, getLoc.longitude)
+    coordinates_df['LATITUDE'] = latitudes
+    coordinates_df['LONGITUDE'] = longitudes
+    msa_df = pd.merge(msa_df, coordinates_df, on ='AREA_TITLE', how ='left')
+    return msa_df
+
+msa_df = bls_df.query('AREA_TYPE == "4"')[['AREA_TITLE', 'OCC_TITLE', 'TOT_EMP', 'LOC_QUOTIENT', 'A_MEAN', 'A_MEDIAN']]
+msa_df = add_rpp(msa_df)
+msa_df = add_coordinates(msa_df)
+msa_df.to_csv('msa.csv', index=False)
